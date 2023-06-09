5a537335b6b9f76ffb52a7ea5566b94762fa881f
suken27 <suken2727@gmail.com>
2023-06-09 19:49:32 +0200

    Add stub network image.

--- src/GraphComponent.js
+++ src/GraphComponent.js
@@ -1,34 +1,77 @@
-import { Component } from "react";
 import * as d3 from 'd3';
+import { Component } from 'react';
 
 class GraphComponent extends Component {
-    
+
     componentDidMount() {
-        this.drawChart();
+        this.drawGraph();
     }
 
-    drawChart() {
-        const data = [12, 5, 6, 6, 9, 10];
+    drawGraph() {
+        const margin = { top: 10, right: 30, bottom: 30, left: 40 },
+            width = 400 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
 
-        const svg = d3.select(".MainScreen")
-                    .append("svg")
-                    .attr("width", 700)
-                    .attr("height", 300);
+        const svg = d3.select('.GraphComponent')
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
 
-        svg.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("x", (d, i) => i * 70)
-            .attr("y", (d, i) => 300 - 10 * d)
-            .attr("width", 65)
-            .attr("height", (d, i) => d * 10)
-            .attr("fill", "green");
+        d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json", function (data) {
+
+            // Initialize the links
+            const link = svg
+                .selectAll("line")
+                .data(data.links)
+                .enter()
+                .append("line")
+                .style("stroke", "#aaa")
+
+            // Initialize the nodes
+            const node = svg
+                .selectAll("circle")
+                .data(data.nodes)
+                .enter()
+                .append("circle")
+                .attr("r", 20)
+                .style("fill", "#69b3a2")
+
+            // Let's list the force we wanna apply on the network
+            const simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+                .force("link", d3.forceLink()                               // This force provides links between nodes
+                    .id(function (d) { return d.id; })                     // This provide  the id of a node
+                    .links(data.links)                                    // and this the list of links
+                )
+                .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+                .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+                .on("end", ticked);
+
+            // This function is run at each iteration of the force algorithm, updating the nodes position.
+            function ticked() {
+                link
+                    .attr("x1", function (d) { return d.source.x; })
+                    .attr("y1", function (d) { return d.source.y; })
+                    .attr("x2", function (d) { return d.target.x; })
+                    .attr("y2", function (d) { return d.target.y; });
+
+                node
+                    .attr("cx", function (d) { return d.x + 6; })
+                    .attr("cy", function (d) { return d.y - 6; });
+            }
+
+        });
     }
 
     render() {
-        return <div id={"#" + this.props.id}></div>
+        return (
+            <div className='GraphComponent'>
+            </div>
+        );
     }
+
 }
 
 export default GraphComponent;
--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,10 +1,14 @@
 import GraphComponent from './GraphComponent';
 import './MainScreen.css'
+import network from './network_diagram.svg'
 
 function MainScreen() {
     return(
         <div className="MainScreen">
-            <GraphComponent/>
+            <div className="MainScreen-content">
+                <img src={network} className='MainScreen-stub' />
+                <GraphComponent/>
+            </div>
         </div>
     );
 }

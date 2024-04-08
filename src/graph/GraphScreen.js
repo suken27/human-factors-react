f529af77c8349654c054a7b2a457e0969d6d02f9
suken27 <suken2727@gmail.com>
2024-04-08 12:12:24 +0200

    Add d3 sample code to GraphScreen.js

--- src/graph/GraphScreen.js
+++ src/graph/GraphScreen.js
@@ -2,9 +2,28 @@ import GraphComponent from "./GraphComponent";
 import "./GraphScreen.css";
 
 function GraphScreen() {
-    return(
-        <GraphComponent />
-    );
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const dataURL =
+      " https://d3js-in-action-third-edition.github.io/hosted-data/apis/front_end_frameworks.json";
+    let mounted = true;
+    d3.json(dataURL).then((data) => {
+      if (mounted) {
+        setData(data);
+        setLoading(false);
+        console.log(data);
+      }
+    });
+    return () => mounted = false;
+  });
+
+  return (
+    <div className="container">
+      {loading && <div className="loading">Loading...</div>}
+      {!loading && <GraphComponent data={data} />}
+    </div>
+  )
 }
 
 export default GraphScreen;
